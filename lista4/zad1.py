d2678340d4ed10b00f1fc3fc647f3536f8c44b0a
francesspot <franciszekpora2@gmail.com>
2025-12-22 00:02:59 +0100

    odkomentowanie kodu

--- lista4/zad1.py
+++ lista4/zad1.py
@@ -54,286 +54,286 @@ img = Image.open(image_path).convert("L")
 image = np.array(img)
 image = image.astype(np.float32) / 255.0
 
-# # Wykrywanie krawędzi za pomocą operatora Sobela
-# gx = convolve2d(image, Sx)
-# gy = convolve2d(image, Sy)
-
-# edges = np.sqrt(gx*gx + gy*gy)
-
-# edges = edges / (edges.max() + 1e-12)
-# edges_out = (edges * 255).astype(np.uint8)
+# Wykrywanie krawędzi za pomocą operatora Sobela
+gx = convolve2d(image, Sx)
+gy = convolve2d(image, Sy)
+
+edges = np.sqrt(gx*gx + gy*gy)
+
+edges = edges / (edges.max() + 1e-12)
+edges_out = (edges * 255).astype(np.uint8)
 
-# plt.figure(figsize=(12, 8))
-
-# plt.subplot(2, 2, 1)
-# plt.title("Oryginał")
-# plt.imshow(img, cmap='gray')
-# plt.axis("off")
+plt.figure(figsize=(12, 8))
+
+plt.subplot(2, 2, 1)
+plt.title("Oryginał")
+plt.imshow(img, cmap='gray')
+plt.axis("off")
 
-# plt.subplot(2, 2, 2)
-# plt.title("Sobel X (gx)")
-# plt.imshow(np.abs(gx), cmap='gray')
-# plt.axis("off")
+plt.subplot(2, 2, 2)
+plt.title("Sobel X (gx)")
+plt.imshow(np.abs(gx), cmap='gray')
+plt.axis("off")
 
-# plt.subplot(2, 2, 3)
-# plt.title("Sobel Y (gy)")
-# plt.imshow(np.abs(gy), cmap='gray')
-# plt.axis("off")
-
-# plt.subplot(2, 2, 4)
-# plt.title("Krawędzie (magnitude)")
-# plt.imshow(edges, cmap='gray')
-# plt.axis("off")
-
-# plt.tight_layout()
-# plt.show()
-
-# #  Wykrywanie krawędzi za pomocą detektora Sobela (suma wartości bezwzględnych)
-# sobel_sum = np.abs(gx) + np.abs(gy)
-# sobel_sum = sobel_sum / (sobel_sum.max() + 1e-12)
-# sobel_sum_out = (sobel_sum * 255).astype(np.uint8)
-
-# plt.figure(figsize=(12, 6))
+plt.subplot(2, 2, 3)
+plt.title("Sobel Y (gy)")
+plt.imshow(np.abs(gy), cmap='gray')
+plt.axis("off")
+
+plt.subplot(2, 2, 4)
+plt.title("Krawędzie (magnitude)")
+plt.imshow(edges, cmap='gray')
+plt.axis("off")
+
+plt.tight_layout()
+plt.show()
+
+#  Wykrywanie krawędzi za pomocą detektora Sobela (suma wartości bezwzględnych)
+sobel_sum = np.abs(gx) + np.abs(gy)
+sobel_sum = sobel_sum / (sobel_sum.max() + 1e-12)
+sobel_sum_out = (sobel_sum * 255).astype(np.uint8)
+
+plt.figure(figsize=(12, 6))
 
-# plt.subplot(1, 2, 1)
-# plt.title("Oryginał")
-# plt.imshow(img, cmap='gray')
-# plt.axis("off")
-
-# plt.subplot(1, 2, 2)
-# plt.title("Detektor Sobela (|gx| + |gy|)")
-# plt.imshow(sobel_sum, cmap='gray')
-# plt.axis("off")
-
-# plt.tight_layout()
-# plt.show()
-
-
-# # Wykrywanie krawędzi za pomocą operatora Laplace'a
-# laplace = convolve2d(image, L)
-# laplace_abs = np.abs(laplace)
-# laplace_edges = laplace_abs / (laplace_abs.max() + 1e-12)
-# laplace_out = (laplace_edges * 255).astype(np.uint8)
-
-# plt.figure(figsize=(12, 6))
-# plt.subplot(1, 2, 1)
-# plt.title("Oryginał")
-# plt.imshow(img, cmap='gray')
-# plt.axis("off")
-
-# plt.subplot(1, 2, 2)
-# plt.title("Krawędzie (Laplace)")
-# plt.imshow(laplace_edges, cmap='gray')
-# plt.axis("off")
-
-# plt.tight_layout()
-# plt.show()
-
-# # Wykrywanie krawędzi za pomocą operatora Scharra
-# gx_scharr = convolve2d(image, Scharr_x)
-# gy_scharr = convolve2d(image, Scharr_y)
-
-# edges_scharr = np.sqrt(gx_scharr*gx_scharr + gy_scharr*gy_scharr)
-# edges_scharr = edges_scharr / (edges_scharr.max() + 1e-12)
-
-# plt.figure(figsize=(12, 8))
-
-# plt.subplot(2, 2, 1)
-# plt.title("Oryginał")
-# plt.imshow(img, cmap='gray')
-# plt.axis("off")
-
-# plt.subplot(2, 2, 2)
-# plt.title("Scharr X (gx)")
-# plt.imshow(np.abs(gx_scharr), cmap='gray')
-# plt.axis("off")
-
-# plt.subplot(2, 2, 3)
-# plt.title("Scharr Y (gy)")
-# plt.imshow(np.abs(gy_scharr), cmap='gray')
-# plt.axis("off")
-
-# plt.subplot(2, 2, 4)
-# plt.title("Krawędzie (Scharr)")
-# plt.imshow(edges_scharr, cmap='gray')
-# plt.axis("off")
-
-# plt.tight_layout()
-# plt.show()
-
-# # Wykrywanie krawędzi za pomocą operatora Prewitta
-# gx_prewitt = convolve2d(image, Prewitt_x)
-# gy_prewitt = convolve2d(image, Prewitt_y)
-
-# edges_prewitt = np.sqrt(gx_prewitt*gx_prewitt + gy_prewitt*gy_prewitt)
-# edges_prewitt = edges_prewitt / (edges_prewitt.max() + 1e-12)
-# edges_prewitt_out = (edges_prewitt * 255).astype(np.uint8)
-
-# plt.figure(figsize=(12, 8))
-
-# plt.subplot(2, 2, 1)
-# plt.title("Oryginał")
-# plt.imshow(img, cmap='gray')
-# plt.axis("off")
-
-# plt.subplot(2, 2, 2)
-# plt.title("Prewitt X (gx)")
-# plt.imshow(np.abs(gx_prewitt), cmap='gray')
-# plt.axis("off")
-
-# plt.subplot(2, 2, 3)
-# plt.title("Prewitt Y (gy)")
-# plt.imshow(np.abs(gy_prewitt), cmap='gray')
-# plt.axis("off")
-
-# plt.subplot(2, 2, 4)
-# plt.title("Krawędzie (Prewitt)")
-# plt.imshow(edges_prewitt, cmap='gray')
-# plt.axis("off")
-
-# plt.tight_layout()
-# plt.show()
-
-# # Porównanie metod wykrywania krawędzi: Oryginał vs Sobel vs Detektor Sobela vs Laplace vs Scharr vs Prewitt
-# plt.figure(figsize=(18, 10))
-
-# plt.subplot(2, 3, 1)
-# plt.title("Oryginał")
-# plt.imshow(img, cmap='gray')
-# plt.axis("off")
-
-# plt.subplot(2, 3, 2)
-# plt.title("Krawędzie Sobel")
-# plt.imshow(edges, cmap='gray')
-# plt.axis("off")
-
-# plt.subplot(2, 3, 3)
-# plt.title("Detektor Sobela (|gx| + |gy|)")
-# plt.imshow(sobel_sum, cmap='gray')
-# plt.axis("off")
-
-# plt.subplot(2, 3, 4)
-# plt.title("Krawędzie (Laplace)")
-# plt.imshow(laplace_edges, cmap='gray')
-# plt.axis("off")
-
-# plt.subplot(2, 3, 5)
-# plt.title("Krawędzie (Scharr)")
-# plt.imshow(edges_scharr, cmap='gray')
-# plt.axis("off")
-
-# plt.subplot(2, 3, 6)
-# plt.title(" Krawędzie (Prewitt)")
-# plt.imshow(edges_prewitt, cmap='gray')
-# plt.axis("off")
-
-# plt.tight_layout()
-# plt.show()
-
-# # Rozmycie Gaussa
-# blurred = convolve2d(image, G)
-# blurred = np.clip(blurred, 0, 1)
-# blurred = (blurred * 255).astype(np.uint8)
-
-# plt.figure(figsize=(12, 6))
-
-# plt.subplot(1, 2, 1)
-# plt.title("Oryginał")
-# plt.imshow(img, cmap='gray')
-# plt.axis("off")
-
-# plt.subplot(1, 2, 2)
-# plt.title("Rozmycie Gaussa")
-# plt.imshow(blurred, cmap='gray')
-# plt.axis("off")
-
-# plt.tight_layout()
-# plt.show()
-
-# # Rozmycie pudełkowe
-# box_blurred = convolve2d(image, box_blur)
-# box_blurred = np.clip(box_blurred, 0, 1)
-# box_blurred = (box_blurred * 255).astype(np.uint8)
-
-# plt.figure(figsize=(12, 6))
-
-# plt.subplot(1, 2, 1)
-# plt.title("Oryginał")
-# plt.imshow(img, cmap='gray')
-# plt.axis("off")
-
-# plt.subplot(1, 2, 2)
-# plt.title("Rozmycie pudełkowe (Box blur)")
-# plt.imshow(box_blurred, cmap='gray')
-# plt.axis("off")
-
-# plt.tight_layout()
-# plt.show()
-
-# # Maskowanie nieostrości
-# unsharp_blurred = convolve2d(image, unsharp_masking)
-# unsharp_blurred = np.clip(unsharp_blurred, 0, 1)
-# unsharp_blurred = (unsharp_blurred * 255).astype(np.uint8)
-
-# plt.figure(figsize=(12, 6))
-# plt.subplot(1, 2, 1)
-# plt.title("Oryginał")
-# plt.imshow(img, cmap='gray')
-# plt.axis("off")
-
-# plt.subplot(1, 2, 2)
-# plt.title("Rozmycie maskowaniem nieostrości (Unsharp masking)")
-# plt.imshow(unsharp_blurred, cmap='gray')
-# plt.axis("off")
-
-# plt.tight_layout()
-# plt.show()
-
-# # Porównanie oryginału, rozmycia Gaussa, rozmycia pudełkowego oraz maskowania nieostrości
-# plt.figure(figsize=(18, 10))
-
-# plt.subplot(2, 2, 1)
-# plt.title("Oryginał")
-# plt.imshow(img, cmap='gray')
-# plt.axis("off")
-
-# plt.subplot(2, 2, 2)
-# plt.title("Rozmycie Gaussa")
-# plt.imshow(blurred, cmap='gray')
-# plt.axis("off")
-
-# plt.subplot(2, 2, 3)
-# plt.title("Rozmycie pudełkowe (Box blur)")
-# plt.imshow(box_blurred, cmap='gray')
-# plt.axis("off")
-
-# plt.subplot(2, 2, 4)
-# plt.title("Maskowanie nieostrości (Unsharp masking)")
-# plt.imshow(unsharp_blurred, cmap='gray')
-# plt.axis("off")
-
-# plt.tight_layout()
-# plt.show()
-
-# # Wyostrzenie obrazu
-# sharpened = convolve2d(image, W)
-# sharpened = np.clip(sharpened, 0, 1)
-# sharpened = (sharpened * 255).astype(np.uint8)
-
-# plt.figure(figsize=(12, 6))
-
-# plt.subplot(1, 2, 1)
-# plt.title("Oryginał")
-# plt.imshow(img, cmap='gray')
-# plt.axis("off")
-
-# plt.subplot(1, 2, 2)
-# plt.title("Wyostrzenie obrazu")
-# plt.imshow(sharpened, cmap='gray')
-# plt.axis("off")
-
-# plt.tight_layout()
-# plt.show()
+plt.subplot(1, 2, 1)
+plt.title("Oryginał")
+plt.imshow(img, cmap='gray')
+plt.axis("off")
+
+plt.subplot(1, 2, 2)
+plt.title("Detektor Sobela (|gx| + |gy|)")
+plt.imshow(sobel_sum, cmap='gray')
+plt.axis("off")
+
+plt.tight_layout()
+plt.show()
+
+
+# Wykrywanie krawędzi za pomocą operatora Laplace'a
+laplace = convolve2d(image, L)
+laplace_abs = np.abs(laplace)
+laplace_edges = laplace_abs / (laplace_abs.max() + 1e-12)
+laplace_out = (laplace_edges * 255).astype(np.uint8)
+
+plt.figure(figsize=(12, 6))
+plt.subplot(1, 2, 1)
+plt.title("Oryginał")
+plt.imshow(img, cmap='gray')
+plt.axis("off")
+
+plt.subplot(1, 2, 2)
+plt.title("Krawędzie (Laplace)")
+plt.imshow(laplace_edges, cmap='gray')
+plt.axis("off")
+
+plt.tight_layout()
+plt.show()
+
+# Wykrywanie krawędzi za pomocą operatora Scharra
+gx_scharr = convolve2d(image, Scharr_x)
+gy_scharr = convolve2d(image, Scharr_y)
+
+edges_scharr = np.sqrt(gx_scharr*gx_scharr + gy_scharr*gy_scharr)
+edges_scharr = edges_scharr / (edges_scharr.max() + 1e-12)
+
+plt.figure(figsize=(12, 8))
+
+plt.subplot(2, 2, 1)
+plt.title("Oryginał")
+plt.imshow(img, cmap='gray')
+plt.axis("off")
+
+plt.subplot(2, 2, 2)
+plt.title("Scharr X (gx)")
+plt.imshow(np.abs(gx_scharr), cmap='gray')
+plt.axis("off")
+
+plt.subplot(2, 2, 3)
+plt.title("Scharr Y (gy)")
+plt.imshow(np.abs(gy_scharr), cmap='gray')
+plt.axis("off")
+
+plt.subplot(2, 2, 4)
+plt.title("Krawędzie (Scharr)")
+plt.imshow(edges_scharr, cmap='gray')
+plt.axis("off")
+
+plt.tight_layout()
+plt.show()
+
+# Wykrywanie krawędzi za pomocą operatora Prewitta
+gx_prewitt = convolve2d(image, Prewitt_x)
+gy_prewitt = convolve2d(image, Prewitt_y)
+
+edges_prewitt = np.sqrt(gx_prewitt*gx_prewitt + gy_prewitt*gy_prewitt)
+edges_prewitt = edges_prewitt / (edges_prewitt.max() + 1e-12)
+edges_prewitt_out = (edges_prewitt * 255).astype(np.uint8)
+
+plt.figure(figsize=(12, 8))
+
+plt.subplot(2, 2, 1)
+plt.title("Oryginał")
+plt.imshow(img, cmap='gray')
+plt.axis("off")
+
+plt.subplot(2, 2, 2)
+plt.title("Prewitt X (gx)")
+plt.imshow(np.abs(gx_prewitt), cmap='gray')
+plt.axis("off")
+
+plt.subplot(2, 2, 3)
+plt.title("Prewitt Y (gy)")
+plt.imshow(np.abs(gy_prewitt), cmap='gray')
+plt.axis("off")
+
+plt.subplot(2, 2, 4)
+plt.title("Krawędzie (Prewitt)")
+plt.imshow(edges_prewitt, cmap='gray')
+plt.axis("off")
+
+plt.tight_layout()
+plt.show()
+
+# Porównanie metod wykrywania krawędzi: Oryginał vs Sobel vs Detektor Sobela vs Laplace vs Scharr vs Prewitt
+plt.figure(figsize=(18, 10))
+
+plt.subplot(2, 3, 1)
+plt.title("Oryginał")
+plt.imshow(img, cmap='gray')
+plt.axis("off")
+
+plt.subplot(2, 3, 2)
+plt.title("Krawędzie Sobel")
+plt.imshow(edges, cmap='gray')
+plt.axis("off")
+
+plt.subplot(2, 3, 3)
+plt.title("Detektor Sobela (|gx| + |gy|)")
+plt.imshow(sobel_sum, cmap='gray')
+plt.axis("off")
+
+plt.subplot(2, 3, 4)
+plt.title("Krawędzie (Laplace)")
+plt.imshow(laplace_edges, cmap='gray')
+plt.axis("off")
+
+plt.subplot(2, 3, 5)
+plt.title("Krawędzie (Scharr)")
+plt.imshow(edges_scharr, cmap='gray')
+plt.axis("off")
+
+plt.subplot(2, 3, 6)
+plt.title(" Krawędzie (Prewitt)")
+plt.imshow(edges_prewitt, cmap='gray')
+plt.axis("off")
+
+plt.tight_layout()
+plt.show()
+
+# Rozmycie Gaussa
+blurred = convolve2d(image, G)
+blurred = np.clip(blurred, 0, 1)
+blurred = (blurred * 255).astype(np.uint8)
+
+plt.figure(figsize=(12, 6))
+
+plt.subplot(1, 2, 1)
+plt.title("Oryginał")
+plt.imshow(img, cmap='gray')
+plt.axis("off")
+
+plt.subplot(1, 2, 2)
+plt.title("Rozmycie Gaussa")
+plt.imshow(blurred, cmap='gray')
+plt.axis("off")
+
+plt.tight_layout()
+plt.show()
+
+# Rozmycie pudełkowe
+box_blurred = convolve2d(image, box_blur)
+box_blurred = np.clip(box_blurred, 0, 1)
+box_blurred = (box_blurred * 255).astype(np.uint8)
+
+plt.figure(figsize=(12, 6))
+
+plt.subplot(1, 2, 1)
+plt.title("Oryginał")
+plt.imshow(img, cmap='gray')
+plt.axis("off")
+
+plt.subplot(1, 2, 2)
+plt.title("Rozmycie pudełkowe (Box blur)")
+plt.imshow(box_blurred, cmap='gray')
+plt.axis("off")
+
+plt.tight_layout()
+plt.show()
+
+# Maskowanie nieostrości
+unsharp_blurred = convolve2d(image, unsharp_masking)
+unsharp_blurred = np.clip(unsharp_blurred, 0, 1)
+unsharp_blurred = (unsharp_blurred * 255).astype(np.uint8)
+
+plt.figure(figsize=(12, 6))
+plt.subplot(1, 2, 1)
+plt.title("Oryginał")
+plt.imshow(img, cmap='gray')
+plt.axis("off")
+
+plt.subplot(1, 2, 2)
+plt.title("Rozmycie maskowaniem nieostrości (Unsharp masking)")
+plt.imshow(unsharp_blurred, cmap='gray')
+plt.axis("off")
+
+plt.tight_layout()
+plt.show()
+
+# Porównanie oryginału, rozmycia Gaussa, rozmycia pudełkowego oraz maskowania nieostrości
+plt.figure(figsize=(18, 10))
+
+plt.subplot(2, 2, 1)
+plt.title("Oryginał")
+plt.imshow(img, cmap='gray')
+plt.axis("off")
+
+plt.subplot(2, 2, 2)
+plt.title("Rozmycie Gaussa")
+plt.imshow(blurred, cmap='gray')
+plt.axis("off")
+
+plt.subplot(2, 2, 3)
+plt.title("Rozmycie pudełkowe (Box blur)")
+plt.imshow(box_blurred, cmap='gray')
+plt.axis("off")
+
+plt.subplot(2, 2, 4)
+plt.title("Maskowanie nieostrości (Unsharp masking)")
+plt.imshow(unsharp_blurred, cmap='gray')
+plt.axis("off")
+
+plt.tight_layout()
+plt.show()
+
+# Wyostrzenie obrazu
+sharpened = convolve2d(image, W)
+sharpened = np.clip(sharpened, 0, 1)
+sharpened = (sharpened * 255).astype(np.uint8)
+
+plt.figure(figsize=(12, 6))
+
+plt.subplot(1, 2, 1)
+plt.title("Oryginał")
+plt.imshow(img, cmap='gray')
+plt.axis("off")
+
+plt.subplot(1, 2, 2)
+plt.title("Wyostrzenie obrazu")
+plt.imshow(sharpened, cmap='gray')
+plt.axis("off")
+
+plt.tight_layout()
+plt.show()
 
 # ============================================
 # DEMOZAIKOWANIE - Filtr Bayera (2x2)
